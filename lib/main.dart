e65905d2c176e125c7f0a214372986693ef4c9c5
Genialngash <ngangadanche@gmail.com>
2022-11-29 18:24:36 +0300

    init services

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:provider/provider.dart' as provider;
+import 'package:sasa_play/screens/main_screens/main_screen.dart';
 import 'package:sasa_play/screens/main_screens/splash_screen.dart';
 import 'package:sasa_play/utils/custom_themes_colors.dart';
+import 'package:sasa_play/utils/service_locator_init.dart';
 
 import 'helpers/theme_changer.dart';
 
-
-void main()async {
+void main() async {
   await GetStorage.init();
   runApp(const MyApp());
+  setup();
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -22,7 +25,6 @@ class MyApp extends StatelessWidget {
       provider.ChangeNotifierProvider(
         create: ((context) => ThemeChange()),
       ),
-    
     ], child: const MyMaterialApp());
   }
 }
@@ -33,12 +35,11 @@ class MyMaterialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'County services app',
+        title: 'Sasa play.',
         themeMode: provider.Provider.of<ThemeChange>(context, listen: true)
             .getCurrentThemeMode(),
         darkTheme: CustomThemeBlue().darkTheme,
         theme: CustomThemeBlue().lightTheme,
-        home: const MysplashScreen());
+        home: const MainScreen());
   }
 }
-
